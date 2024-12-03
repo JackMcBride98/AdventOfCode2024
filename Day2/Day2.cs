75afb7d74936b8a7205d535a218bdc96fd8c33bf
Jack McBride <jack.mcbride@ghyston.com>
2024-12-03 10:16:58 +0000

    Day 2 Part 2

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -7,18 +7,58 @@ public class Day2() : SolutionBase("Day2")
         var lines = isExample ? ExampleLines : InputLines;
         var numbers = lines.Where(line => !string.IsNullOrEmpty(line)).Select(line => line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)).Select(v => v.Select(int.Parse).ToList()).ToList();
 
-        var reportSafeList = numbers.Select(IsReportSafe).ToList();
+        var reportSafeList = numbers.Select(IsReportSafePart1).ToList();
 
         return reportSafeList.Count(v => v).ToString();
     }
 
-    private static bool IsReportSafe(List<int> numbers)
+    public override string Part2(bool isExample = false)
     {
-        var isAllIncreasing = numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] > n).All(v => v);
-        var isAllDecreasing = numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] < n).All(v => v);
+        var lines = isExample ? ExampleLines : InputLines;
+        var numbers = lines.Where(line => !string.IsNullOrEmpty(line)).Select(line => line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)).Select(v => v.Select(int.Parse).ToList()).ToList();
 
-        var areAllDifferencesBetween1And3 = numbers.Take(numbers.Count - 1).Select((n, i) => Math.Abs(numbers[i + 1] - n)).All(v => v >= 1 && v <= 3);
+        var reportSafeList = numbers.Select(IsReportSafePart2).ToList();
 
-        return (isAllIncreasing || isAllDecreasing) && areAllDifferencesBetween1And3;
+        return reportSafeList.Count(v => v).ToString();
+    }
+
+    private static bool IsReportSafePart1(List<int> numbers)
+    {
+        return (IsAllIncreasing(numbers) || IsAllDecreasing(numbers)) && AreAllDifferencesBetweenOneAndThree(numbers);
+    }
+
+    private static bool IsReportSafePart2(List<int> numbers)
+    {
+        if (IsReportSafePart1(numbers))
+        {
+            return true;
+        }
+
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            var numbersWithoutIndex = numbers.Where((_, index) => index != i).ToList();
+
+            if (IsReportSafePart1(numbersWithoutIndex))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool AreAllDifferencesBetweenOneAndThree(List<int> numbers)
+    {
+        return numbers.Take(numbers.Count - 1).Select((n, i) => Math.Abs(numbers[i + 1] - n)).All(v => v >= 1 && v <= 3);
+    }
+
+    private static bool IsAllIncreasing(List<int> numbers)
+    {
+        return numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] > n).All(v => v);
+    }
+
+    private static bool IsAllDecreasing(List<int> numbers)
+    {
+        return numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] < n).All(v => v);
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ var day = new Day2();
 
 var part1 = day.Part1();
 Console.WriteLine(part1);
-//
-// var part2 = day.Part2();
-// Console.WriteLine(part2);
+
+var part2 = day.Part2();
+Console.WriteLine(part2);
