f4e8c7ab03c374d45a1ad06cf9ba38e02ba1ecd7
Jack McBride <jack.mcbride@ghyston.com>
2024-12-03 10:54:36 +0000

    Day 3 Part 1

---
+++ Day3/Day3.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day3;
+
+public class Day3() : SolutionBase("Day3")
+{
+    public override string Part1(bool isExample = false)
+    {
+        var lines = isExample ? ExampleLines : InputLines;
+
+        var mulRegex = new Regex(@"mul\(\d+,\d+\)");
+        var numRegex = new Regex(@"\d+");
+
+        var mulMatches = lines.Where(line => !string.IsNullOrEmpty(line)).Select(line => mulRegex.Matches(line));
+
+        var total = 0;
+
+        foreach (var matches in mulMatches)
+        {
+            foreach (var match in matches)
+            {
+                var numbers = numRegex.Matches(match.ToString()!).ToList().Select(m => int.Parse( m.Value)).ToList();
+                total += numbers[0] * numbers[1];
+            }
+        }
+        return total.ToString();
+    }
+
+    public override string Part2(bool isExample = false)
+    {
+        return "Part 2";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿
-using AdventOfCode2024.Day2;
+﻿using AdventOfCode2024.Day3;
 
-var day = new Day2();
+var day = new Day3();
 
 var part1 = day.Part1();
 Console.WriteLine(part1);
 
-var part2 = day.Part2();
-Console.WriteLine(part2);
+// var part2 = day.Part2();
+// Console.WriteLine(part2);
