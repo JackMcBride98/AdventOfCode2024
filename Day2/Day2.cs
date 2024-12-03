1aac5c2032968cb9ef932b461e8b90893f7ba86b
Jack McBride <jack.mcbride@ghyston.com>
2024-12-03 09:43:31 +0000

    Day 2 Part 1

---
+++ Day2/Day2.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2024.Day2;
+
+public class Day2() : SolutionBase("Day2")
+{
+    public override string Part1(bool isExample = false)
+    {
+        var lines = isExample ? ExampleLines : InputLines;
+        var numbers = lines.Where(line => !string.IsNullOrEmpty(line)).Select(line => line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)).Select(v => v.Select(int.Parse).ToList()).ToList();
+
+        var reportSafeList = numbers.Select(IsReportSafe).ToList();
+
+        return reportSafeList.Count(v => v).ToString();
+    }
+
+    private static bool IsReportSafe(List<int> numbers)
+    {
+        var isAllIncreasing = numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] > n).All(v => v);
+        var isAllDecreasing = numbers.Take(numbers.Count - 1).Select((n, i) => numbers[i + 1] < n).All(v => v);
+
+        var areAllDifferencesBetween1And3 = numbers.Take(numbers.Count - 1).Select((n, i) => Math.Abs(numbers[i + 1] - n)).All(v => v >= 1 && v <= 3);
+
+        return (isAllIncreasing || isAllDecreasing) && areAllDifferencesBetween1And3;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using AdventOfCode2024.Day1;
+﻿
+using AdventOfCode2024.Day2;
 
-var day1 = new Day1();
+var day = new Day2();
 
-var part1 = day1.Part1();
+var part1 = day.Part1();
 Console.WriteLine(part1);
-
-var part2 = day1.Part2();
-Console.WriteLine(part2);
+//
+// var part2 = day.Part2();
+// Console.WriteLine(part2);
