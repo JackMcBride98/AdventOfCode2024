319ae323e384943ff8dfa9b960b74d80ac3b961a
Jack McBride <jack.mcbride@ghyston.com>
2024-12-03 09:23:58 +0000

    Day 1 Part 2

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -17,4 +17,26 @@ public class Day1() : SolutionBase("Day1")
 
         return totalDifference.ToString();
     }
+
+    public override string Part2(bool isExample = false)
+    {
+        var lines = isExample ? ExampleLines : InputLines;
+        var numbers = lines.Where(line => !string.IsNullOrEmpty(line)).Select(line => line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)).Select(v => v.Select(int.Parse).ToList()).ToList();
+
+        var leftNumbers = numbers.Select(row => row.First()).ToList();
+        var rightNumbers = numbers.Select(row => row.Last()).ToList();
+
+        var totalSimilarityScore = 0;
+
+        for (var i  = 0; i < leftNumbers.Count; i++)
+        {
+            var leftNumber = leftNumbers[i];
+
+            var countInRightNumbers = rightNumbers.FindAll(rightNumber => rightNumber == leftNumber).Count;
+
+            totalSimilarityScore += leftNumber * countInRightNumbers;
+        }
+
+        return totalSimilarityScore.ToString();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,3 +4,6 @@ var day1 = new Day1();
 
 var part1 = day1.Part1();
 Console.WriteLine(part1);
+
+var part2 = day1.Part2();
+Console.WriteLine(part2);
